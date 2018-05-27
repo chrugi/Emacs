ab9ca558cdeb6c3b75a24a64b40a498c858454af
Christian Sangvik <christian.sangvik@gmx.ch>
2018-05-27 17:54:43 +0200

    Clean up init.el file

--- init.el
+++ init.el
@@ -16,21 +16,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#0a0814" "#f2241f" "#67b11d" "#b1951d" "#4f97d7" "#a31db1" "#28def0" "#b2b2b2"])
- '(custom-safe-themes
-   (quote
-    ("fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
  '(magit-fetch-arguments (quote ("--prune")))
  '(magit-log-arguments (quote ("--graph" "--decorate" "--show-signature")))
  '(magit-remote-arguments (quote ("-f")))
  '(magit-tag-arguments (quote ("--sign")))
- '(package-selected-packages
-   (quote
-    (spacemacs-theme org-ref wanderlust pcre2el neotree magit org-bullets org-ac iedit elpy paredit ace-window nlinum-relative auto-complete avy counsel aggressive-indent hungry-delete expand-region undo-tree which-key try use-package))))
+ )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:height 3.0)))))
+ )
