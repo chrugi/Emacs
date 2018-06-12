2e1cf69cd9ec3e98aee29c536a2bebce5283d5c8
Melanie Bühler <meli.buehler@gmail.com>
2018-06-12 12:42:58 +0200

    Unify all configuration in Literate Config file

--- init.el
+++ init.el
@@ -1,26 +1,2 @@
-(require 'package)
-(require 'cl)
-(setq package-enable-at-startup nil)
-(setq package-archives
-      '(("gnu" . "https://elpa.gnu.org/packages/")
-	("melpa" . "https://melpa.org/packages/")
-	("org" . "https://orgmode.org/elpa/")))
-(package-initialize)
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
